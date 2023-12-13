7c07b1b547b045b9d41f12be28ad2398c6b490dc
chocogueh <46663189+chocogueh@users.noreply.github.com>
2023-12-14 01:14:55 +0900

    case
    CASE 회전 코드입니다.

---
+++ CASE_Rotater.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class CASE_Rotater : MonoBehaviour
+{
+    private float speed = 1f;
+    private Vector3 lastMousePosition;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            lastMousePosition = Input.mousePosition;
+        }
+        else if (Input.GetMouseButton(0))
+        {
+            Vector3 deltaMouse = Input.mousePosition - lastMousePosition;
+            transform.Rotate(Vector3.up, -deltaMouse.x * speed, Space.World);
+            transform.Rotate(Vector3.right, deltaMouse.y * speed);
+            lastMousePosition = Input.mousePosition;
+        }
+    }
+}
